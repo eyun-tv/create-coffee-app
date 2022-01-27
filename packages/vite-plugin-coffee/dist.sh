34c52294cf7c7f7f2f4b21f02adc5444277311bb
gcxfd <i@rmw.link>
2022-01-27 17:39:12 +0800

    🔶

--- packages/vite-plugin-coffee/dist.sh
+++ packages/vite-plugin-coffee/dist.sh
@@ -5,6 +5,7 @@ _DIR=$(cd "$(dirname "$0")"; pwd)
 cd $_DIR
 
 set -ex
+npm run prepublish
 
 version=$(cat package.json|jq -r '.version')
 
@@ -16,7 +17,6 @@ git commit -m v$version || true
 add
 
 git pull
-
 npm set unsafe-perm true
 npm version patch
 add
